c19975acffd1b639ee25eae0af1447437e01fa67
Andy Grabow <andy@freilandkiwis.de>
2025-09-04 09:09:34 +0200

    docs be gone in this branch

--- dotfiles/docs_src/release.sh
+++
@@ -1,8 +0,0 @@
-unalias rm
-yarn build && rm -rf ../docs && mv dist ../docs
-gsed -i 's#="/#="#g' ../docs/index.html # make the included files work for localhost as well as at /dotfiles/
-gsed -i 's#/fonts#../fonts#g' ../docs/css/*
-git add ../docs ../docs_src
-git commit -m "readme updates"
-git push
-
